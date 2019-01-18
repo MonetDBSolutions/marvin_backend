58b455fd5beee295631b75c226ae1b809a3cecdf
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2019-01-18 16:57:16 +0100

    Add the first endpoint for queries

--- marvin_backend/marvin_backend.py
+++ marvin_backend/marvin_backend.py
@@ -1,3 +1,25 @@
 # -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright MonetDB Solutions B.V. 2018-2019
+from pathlib import Path
+
+import falcon
+from mal_analytics import db_manager
+
+from marvin_backend import queries
 
 """Main module."""
+
+# For development purposes. These need to be set up properly at
+# initialization
+CURR_PATH = Path().cwd()
+DB_PATH = CURR_PATH / 'db_path/dev_db'
+
+api = application = falcon.API()
+db_manager = db_manager.DatabaseManager(str(DB_PATH.resolve()))
+
+queries = queries.Queries(db_manager)
+api.add_route('/queries', queries)
---
+++ marvin_backend/queries.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright MonetDB Solutions B.V. 2018-2019
+import json
+import logging
+
+import falcon 
+
+LOGGER = logging.getLogger(__name__)
+
+class Queries(object):
+    def __init__(self, db):
+        self._db = db
+
+    def on_get(self, req, resp):
+        all_queries_q = "SELECT * from query"
+        all_queries = self._db.execute_query(all_queries_q)
+        result = dict([(k, v.tolist()) for k, v in all_queries.items()])
+
+        doc = {
+            'links': {
+                'url': req.url,
+            },
+            'data': result,
+        }
+
+        resp.body = json.dumps(doc, ensure_ascii=False)
+
+        resp.status = falcon.HTTP_200
