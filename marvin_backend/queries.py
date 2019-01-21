f19a7c8280db159549800b95d4ca8d5175d54229
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2019-01-21 17:45:01 +0100

    Handle JSON encoding of numpy types
    We need to use a custom JSON encoder that converts the numpy types to
    plain Python types that the json package can handle. See
    
    https://github.com/mpld3/mpld3/issues/434#issuecomment-340255689
    and
    https://stackoverflow.com/a/49677241

--- marvin_backend/queries.py
+++ marvin_backend/queries.py
@@ -13,6 +13,7 @@ from marvin_backend import utils
 
 LOGGER = logging.getLogger(__name__)
 
+
 class Queries(object):
     def __init__(self, db):
         self._db = db
@@ -20,10 +21,7 @@ class Queries(object):
     def on_get(self, req, resp):
         all_queries_q = "SELECT * from query"
         all_queries = self._db.execute_query(all_queries_q)
-        # numpy arrays are not JSON serializable, so convert
-        # everything to a normal python list
-        result_t = dict([(k, v.tolist()) for k, v in all_queries.items()])
-        result = utils.DLtoLD(result_t)
+        result = utils.DLtoLD(all_queries)
 
         doc = {
             'links': {
@@ -32,6 +30,6 @@ class Queries(object):
             'data': result,
         }
 
-        resp.body = json.dumps(doc, ensure_ascii=False)
+        resp.body = json.dumps(doc, ensure_ascii=False, cls=utils.NumpyJSONEncoder)
 
         resp.status = falcon.HTTP_200
--- marvin_backend/utils.py
+++ marvin_backend/utils.py
@@ -4,7 +4,8 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 # Copyright MonetDB Solutions B.V. 2018-2019
-
+import json
+import numpy as np
 
 def DLtoLD(dl):
     """Turn a dict of lists into a list of dicts
@@ -54,3 +55,22 @@ row based to column based.
 """
 
     return {k: [dic[k] for dic in ld] for k in ld[0]}
+
+
+# numpy types are not JSON serializable, so we need to convert
+# everything to a normal python type. Unfortunately this does not work
+# with ujson, and this might create a performance issue down the line.
+class NumpyJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+                            np.int16, np.int32, np.int64, np.uint8,
+                            np.uint16, np.uint32, np.uint64)):
+            return int(obj)
+        elif isinstance(obj, (np.float_, np.float16, np.float32,
+                              np.float64)):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            # this works for masked arrays as well because the masked
+            # array is a subtype of np.ndarray
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
