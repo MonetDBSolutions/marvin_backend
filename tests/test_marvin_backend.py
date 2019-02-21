771a90f454d7942160a165d485291a69a94add24
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2019-02-21 17:53:31 +0100

    Start testing properly with mocked db connection

--- tests/test_marvin_backend.py
+++ tests/test_marvin_backend.py
@@ -3,23 +3,59 @@
 
 """Tests for `marvin_backend` package."""
 
+import falcon
+from falcon import testing
+from numpy import array
+from numpy.ma import masked_array
 import pytest
-
+from unittest.mock import call, MagicMock, mock_open
 
 from marvin_backend import marvin_backend
 
+@pytest.fixture
+def mock_db():
+    return MagicMock()
+
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def client(mock_db):
+    api = marvin_backend.create_app(mock_db)
+    return testing.TestClient(api)
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_queries(client, mock_db):
+    query_result = {
+        "query_id": array([1, 2]),
+        "query_text": array(["SELECT * FROM foo;", "SELECT * FROM bar;"]),
+        "query_label": masked_array(data=["null label", "non null label"], mask=[True, False]),
+        "root_execution_id": array([1, 40])
+    }
+    mock_db.execute_query.return_value = query_result
+
+    doc = {
+        "data": [
+            {
+                "query_id": 1,
+                "query_label": None,
+                "query_text": "SELECT * FROM foo;",
+                "root_execution_id": 1
+            },
+            {
+                "query_id": 2,
+                "query_text": "SELECT * FROM bar;",
+                "query_label": "non null label",
+                "root_execution_id": 40
+            }
+        ],
+        "data_length": 2,
+        "links": {
+            "url": "http://falconframework.org/queries"
+        }
+    }
+
+    response = client.simulate_get('/queries')
+    result_doc = response.json
+
+    print(result_doc)
+    assert result_doc == doc
+    assert response.status == falcon.HTTP_OK
